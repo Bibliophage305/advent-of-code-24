3a973ed11dd3210621479f63dce74ea1582b2d03
Chris <christopher.hampson58@gmail.com>
2024-12-02 13:25:31 +0000

    Pretty

--- advent_of_code/2/solver.py
+++ advent_of_code/2/solver.py
@@ -7,7 +7,7 @@ class Solver(advent.Advent):
 
     def process_data(self, data):
         return [[list(map(int, line.split())) for line in data]]
-    
+
     def is_safe(self, line):
         if line[0] > line[1]:
             return all(0 < a - b < 4 for a, b in zip(line, line[1:]))
@@ -17,4 +17,7 @@ class Solver(advent.Advent):
         return sum(map(self.is_safe, data))
 
     def part_2(self, data):
-        return sum(any(self.is_safe(line[:i] + line[i + 1:]) for i in range(len(line))) for line in data)
+        return sum(
+            any(self.is_safe(line[:i] + line[i + 1 :]) for i in range(len(line)))
+            for line in data
+        )
